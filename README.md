b4f0548f494c0d40e9a532ff4122f1d9b80150e0
Anastasiya Osher <anastasiya.osher@nexenta.com>
2016-11-17 15:59:53 -0800

    Added build instructions for docker image

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nedge-cosbench
+
+
+	Build Docker Image
+1. Create repository clone
+2. Build nexenta/cosbench using instructions in nexenta/cosbench/Build.md url: https://github.com/Nexenta/cosbench/blob/master/BUILD.md
+3. Copy start.sh to cos folder
+4. $ docker build -t nedge-cosbench .
+5. $ docker images
+6. $ docker tag <Image ID> nexenta/cosbench:latest
+7. $ docker login
+8. $ docker push nexenta/cosbench
---
+++ start.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# starting drives and controlles
+
+
+if [ $# -eq 0 ] || [ "$2" = "driver" ]; then
+echo "Starting only driver"
+        ./start-driver.sh
+
+
+while [ ! -f log/driver-boot.log ]; do
+   sleep 10
+done
+
+tail -f log/driver-boot.log
+
+exit
+fi
+
+if  [ "$2" = "controller" ]; then
+echo "Starting only controller"
+
+
+ips=(`echo $1 | sed 's/,/\n/g'`)
+
+n=${#ips[@]}
+echo "$n"
+echo " " >> conf/controller.conf
+echo "[controller]" > conf/controller.conf
+echo "drivers = $n" >> conf/controller.conf
+echo "log_level = INFO" >> conf/controller.conf
+echo "log_file = log/system.log" >> conf/controller.conf
+echo "archive_dir = archive" >> conf/controller.conf
+echo " " >> conf/controller.conf
+echo " " >> conf/controller.conf
+
+        for (( i=0; i<$n; i++))
+do
+echo "[driver$(($i+1))]" >> conf/controller.conf
+echo "name = driver$(($i+1))" >> conf/controller.conf
+echo "url = http://${ips[$i]}:18088/driver" >> conf/controller.conf
+echo " " >> conf/controller.conf
+echo " " >> conf/controller.conf
+
+done
+
+cat conf/controller.conf
+
+./start-controller.sh
+
+while [ ! -f log/system.log ]; do
+   sleep 10
+done
+
+tail -f log/system.log
+
+
+exit
+fi
+
+if  [ "$2" = "both" ]; then
+
+n=1
+if [ "$3x" != "x" ]; then
+n=$3
+fi 
+
+echo " " >> conf/controller.conf
+echo "[controller]" > conf/controller.conf
+echo "drivers = $n" >> conf/controller.conf
+echo "log_level = INFO" >> conf/controller.conf
+echo "log_file = log/system.log" >> conf/controller.conf
+echo "archive_dir = archive" >> conf/controller.conf
+echo " " >> conf/controller.conf
+echo " " >> conf/controller.conf
+
+	for (( i=1; i<$n+1; i++))
+do
+echo "[driver$i]" >> conf/controller.conf
+echo "name = driver$i" >> conf/controller.conf
+echo "url = http://$1:$((18087+$i))/driver" >> conf/controller.conf
+echo " " >> conf/controller.conf
+echo " " >> conf/controller.conf
+
+
+	if [ "$i" -eq "1" ];then
+
+ ./start-driver.sh
+else
+echo " start docker driver port:$((18087+$i))"
+docker run -p "$((18087+$i))":18088  nedgecos &
+
+fi
+done
+
+cat conf/controller.conf
+
+./start-controller.sh
+
+while [ ! -f log/system.log ]; do
+   sleep 10
+done
+
+tail -f log/system.log
+
+exit
+fi
+
